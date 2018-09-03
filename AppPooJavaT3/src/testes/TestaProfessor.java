7a86f304171d8e67c6d9424f70e95992ae358a9a
ThamyresAbal <42938115+ThamyresAbal@users.noreply.github.com>
2018-09-03 10:48:45 -0300

    Update TestaProfessor.java

--- AppPooJavaT3/src/testes/TestaProfessor.java
+++ AppPooJavaT3/src/testes/TestaProfessor.java
@@ -8,31 +8,31 @@ public class TestaProfessor {
 		
 		String nome;
 		System.out.printf("Informe o seu nome: ");		
-		nome = ler.nextLine();		/*pega só o que está na linha*/
+		nome = ler.nextLine();		/*pega sÃ³ o que estÃ¡ na linha*/
 		
 		int matricula;
-		System.out.printf("Informe a sua matrícula: ");		
-		matricula = ler.nextInt();		/*lendo número inteiros*/
+		System.out.printf("Informe a sua matrÃ­cula: ");		
+		matricula = ler.nextInt();		/*lendo nÃºmero inteiros*/
 		
 		float salario;
-		System.out.printf("Informe o seu salário: ");		
+		System.out.printf("Informe o seu salÃ¡rio: ");		
 		salario = ler.nextFloat();		
 	
 		boolean temporario;
-		System.out.printf("Informe se é um professor temporário: ");		
+		System.out.printf("Informe se Ã© um professor temporÃ¡rio: ");		
 		temporario = ler.nextBoolean(); /*true ou false como resposta*/
-		/* Abaixo as condições do operador ternário*/
+		/* Abaixo as condiÃ§Ãµes do operador ternÃ¡rio*/
 		
 		int experiencia;
-		System.out.printf("Quantos meses de experiência? ");
+		System.out.printf("Quantos meses de experiÃªncia? ");
 		experiencia = ler.nextInt();
 		
 		float bonus;
-		System.out.printf("Digite o valor do bônus: ");
+		System.out.printf("Digite o valor do bÃ´nus: ");
 		bonus = ler.nextFloat();
 		
 		float gratificacao;
-		System.out.printf("Digite o valor de gratificação: ");
+		System.out.printf("Digite o valor de gratificaÃ§Ã£o: ");
 		gratificacao = ler.nextFloat();
 		
 		float desconto;
@@ -49,18 +49,18 @@ public class TestaProfessor {
 		final float VL_DISCIPLINA = 100;
 		salarioBruto = (salario + bonus + gratificacao - desconto) + experiencia * 50 + disciplinas * 100;
 		
-
+//materia
 		System.out.println("Nome: " + nome);
-		System.out.println("Matrícula: " + matricula);
-		System.out.println("Salário: " + salario);
-		System.out.println("Temporário? " + (temporario ? "Sim" : "Não"));
-		System.out.println("Experiência: " + experiencia);
+		System.out.println("MatrÃ­cula: " + matricula);
+		System.out.println("SalÃ¡rio: " + salario);
+		System.out.println("TemporÃ¡rio? " + (temporario ? "Sim" : "NÃ£o"));
+		System.out.println("ExperiÃªncia: " + experiencia);
 		System.out.println("Bonus: " + bonus);
-		System.out.println("Gratificação: " + gratificacao);
+		System.out.println("GratificaÃ§Ã£o: " + gratificacao);
 		System.out.println("Desconto: " + desconto);
 		System.out.println("Disciplinas: " + disciplinas);
-		System.out.printf("Salário bruto:  R$%f ", salarioBruto);
-		System.out.printf("O Professor %s (%d) é %s, pois ganha R$%.2f",
+		System.out.printf("SalÃ¡rio bruto:  R$%f ", salarioBruto);
+		System.out.printf("O Professor %s (%d) Ã© %s, pois ganha R$%.2f",
 				nome,
 				matricula,
 				(salarioBruto > 10000 ? "Rico " : "Pobre"),
